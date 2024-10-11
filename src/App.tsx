0120fa70c76d804ab67b03bc9873396f63793d74
RuiJ72 <rlagos900@gmail.com>
2024-10-11 16:03:38 -0300

    Adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { z, ZodType } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import "./App.css";
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  age: number;
+  password: string;
+  confirmPassword: string;
+};
 
 function App() {
+  const [message, setMessage] = useState(""); // State for success message
+
+  const schema: ZodType<FormData> = z
+    .object({
+      firstName: z.string().min(2).max(30),
+      lastName: z.string().min(2).max(30),
+      email: z.string().email(),
+      age: z.number().min(18).max(70),
+      password: z.string().min(5).max(20),
+      confirmPassword: z.string().min(5).max(20),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const submitData = (data: FormData) => {
+    setMessage("You are now logged in!"); // Set success message
+    console.log("IT WORKED", data); // Message to be seen at the console
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(submitData)}>
+        <label> First Name </label>
+        <input type="text" {...register("firstName")} />
+        {errors.firstName && <span> {errors.firstName.message}</span>}
+        <label> Last Name </label>
+        <input type="text" {...register("lastName")} />
+        {errors.lastName && <span> {errors.lastName.message}</span>}
+        <label> Email </label>
+        <input type="email" {...register("email")} />
+        {errors.email && <span> {errors.email.message}</span>}
+        <label> Age </label>
+        <input type="number" {...register("age", { valueAsNumber: true })} />
+        {errors.age && <span> {errors.age.message}</span>}
+        <label> Password </label>
+        <input type="password" {...register("password")} />
+        {errors.password && <span> {errors.password.message}</span>}
+        <label> Confirm Password </label>
+        <input type="password" {...register("confirmPassword")} />
+        {errors.confirmPassword && (
+          <span> {errors.confirmPassword.message}</span>
+        )}
+
+        <input type="submit" />
+      </form>
+      
+      {message && <p>{message}</p>}
     </div>
   );
 }
